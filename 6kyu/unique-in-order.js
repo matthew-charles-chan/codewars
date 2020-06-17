25cd876c37f9698404b48d3877ff7b5997d19f68
Matthew Charles Chan <matthew.charles.chan@gmail.com>
2020-06-17 12:20:40 -0600

    update solution

---
+++ 6kyu/unique-in-order.js
@@ -0,0 +1,14 @@
+const uniqueInOrder = (iterable) => {
+  const output = [];
+  let last;
+  // const output = iterable[0]? [iterable[0]]: []
+  for (let i = 1; i < iterable.length; i ++) {
+    if( iterable[i] !== last) {
+      output.push(last = iterable[i]);
+    }
+  }
+  return output;
+}
+
+console.log(uniqueInOrder('AAAABBBCCDAABBB'));
+console.log(uniqueInOrder([1,2,2,3,3]));
