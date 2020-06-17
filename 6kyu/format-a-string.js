1cc2ca10da71d473695cd7785780db423787b908
Matthew Charles Chan <matthew.charles.chan@gmail.com>
2020-06-16 20:06:36 -0600

    kata join names

---
+++ 6kyu/format-a-string.js
@@ -0,0 +1,29 @@
+const list = (names) => {
+  // let namesArr = [];
+  let namesStr = "";
+  if (names.length === 1) {
+    namesStr = names[0].name;
+  }
+  else {
+    for(let i = 0; i < names.length; i ++) {
+      if (i > names.length - 2) {
+        namesStr += names[i].name
+      } else {
+        namesStr += names[i].name 
+        i > names.length - 3? 
+        namesStr += " & ":
+        namesStr += ", ";
+      }
+    }
+  }
+  return namesStr;
+}
+
+function betterList(names) {
+  var xs = names.map(p => p.name)
+  var x = xs.pop()
+  return xs.length ? xs.join(", ") + " & " + x : x || ""
+}
+
+
+console.log(list([{name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ]))
