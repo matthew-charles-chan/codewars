12274c0b25c39633174eeff1a3b2b55591b408e4
Matthew Charles Chan <matthew.charles.chan@gmail.com>
2020-06-19 16:41:54 -0600

    snail with modifying

--- 4kyu/snail.js
+++ 4kyu/snail.js
@@ -1,7 +1,7 @@
 
 // const parse = (arr, start, end) => {
 //   const output = [];
-//   // output = [...output, ...arr.slice()]
+
 //   for (let i = start; i <= end; i ++) {
 //     output.push(arr[start][i]);
 //   }
@@ -25,27 +25,27 @@
 //   return output.concat(parse(arr, start + 1, end - 1));
 // };
 
-// const snail = (arr) => {
-//   if (arr[0].length === 0)  return [];
-//   return parse(arr, 0, arr.length - 1);
-// }
 
 const snail = (arr) => {
-  // if (arr[0].length === 0)  return [];
-  // return parse(arr, 0, arr.length - 1);
-  let result;
-  while (arr.length) {
-    result = (result ? result.concat(arr.shift()) : arr.shift());
+  const result = [];
 
+  while (arr.length) {
+    // get N row and remove from arr
+    result.push(...arr.shift());
+
+    // get W row and remove from arr
     for (let i = 0; i < arr.length; i ++) {
       result.push(arr[i].pop());
     }
 
-    result = result.concat((arr.pop() || []).reverse());
-
+    // get S row and remove from arr, if arr is empty, push empty arr
+    result.push(...(arr.pop() || []).reverse());
+    
+    // get E rown and remove from arr
     for (let i = arr.length - 1; i >= 0; i --) {
       result.push(arr[i].shift());
     }
+    
   }
   return result;
 
@@ -72,5 +72,5 @@ const testArr3 = [
 // console.log(snail(testArr) === [1,2,3,4,5,6,7,8,9]);
 console.log(snail(testArr));
 console.log(snail(testArr2));
-// console.log(snail(testArr3));
+console.log(snail(testArr3));
          
