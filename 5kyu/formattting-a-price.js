35254659be4b0221bf8c146fb60869d6eaa7812a
Matthew Charles Chan <matthew.charles.chan@gmail.com>
2020-07-08 10:37:44 -0700

    using toLocaleString and for loop to create price string

---
+++ 5kyu/formattting-a-price.js
@@ -0,0 +1,23 @@
+const numberToPrice = (number) => {
+  if (typeof number !== "number") {
+    return 'NaN';
+  }
+  const numStr = number.toLocaleString();
+  const [whole, decimal] = numStr.split(".");
+  
+  let trunkatedDecimal = "";
+
+  if (decimal) {
+    for (let i = 0; i < 2; i ++) {
+      decimal[i] ? trunkatedDecimal += decimal[i] : trunkatedDecimal += "0";
+    }
+  } else {
+    trunkatedDecimal = "00";
+  }
+
+  const price = whole + "." + trunkatedDecimal;
+  return price;
+};
+console.log(numberToPrice(1500.129));
+console.log(numberToPrice(-5));
+console.log(numberToPrice(1000000.5));
