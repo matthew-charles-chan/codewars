329053d31f5f871d0e09ad53dac5612f70468686
Matthew Charles Chan <matthew.charles.chan@gmail.com>
2020-07-08 09:45:04 -0700

    new codewars

---
+++ 5kyu/best-match.js
@@ -0,0 +1,27 @@
+const  bestMatch = (aGoals, bGoals) => {
+  const goalDiffs = [];
+  for (let i = 0; i < aGoals.length; i ++) {
+    goalDiffs.push(aGoals[i] - bGoals[i]);
+  }
+
+  const minDiff = Math.min(...goalDiffs);
+
+  const closeGames = [];
+  for (let i = 0; i < goalDiffs.length; i ++) {
+    goalDiffs[i] === minDiff ?
+      closeGames.push(bGoals[i]) :
+      closeGames.push(-1);
+  }
+  return (closeGames.indexOf(Math.max(...closeGames)));
+};
+
+// console.log(bestMatch([6, 4],[1, 2]));
+
+// console.log(bestMatch([1],[0]));
+
+// console.log(bestMatch([1, 2, 3, 4, 5],[0, 1, 2, 3, 4]));
+
+// console.log(bestMatch([3, 4, 3],[1, 1, 2]));
+
+// console.log(bestMatch([4, 3, 4],[1, 1, 1]));
+
